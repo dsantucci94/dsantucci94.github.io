f6a7c82795d64ab6c0b88506a4377b663371eb38
Danielle Santucci <daniellesantucci@Danielles-MBP.wireless.newschool.edu>
2018-02-13 11:35:20 -0500

    JS

---
+++ labs/lab-4a/assets/js/app.js
@@ -0,0 +1,18 @@
+function hello (name, age) {
+  console.log (name);
+  console.log (age);
+}
+
+hello ('Danielle Santucci', 17);
+
+// var first_name = 'Danielle',
+//     last_name  ='Santucci',
+//     full_name  = first_name + ' ' + last_name;
+
+// function hello() {
+  
+//   alert (full_name);
+  
+// }
+
+// hello();
---
+++ labs/lab-4b/assets/js/app.js
@@ -0,0 +1,16 @@
+$('.item').click(function(){
+   if (!$(this).hasClass('in-cart')){
+     alert('ADDED TO CART');
+
+     var current_count = $('.item-count').html(),
+     current_value = Math.abs(current_count),
+     new_count         = current_value + 1;
+   
+     $('.item-count').html(new_count);
+     $(this).addClass('in-cart');
+
+   }
+
+});
+
+
